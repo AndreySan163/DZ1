f3430c9aa8d30473c10c679ecc870111e012b1ab
Andrei <nestorivanov163@gmail.com>
2023-10-20 14:49:19 +0400

    DZ1--Lesson2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите наименование товара: ");
+        String product = scanner.nextLine();
+        System.out.print("Введите количество товара: ");
+        int quantity = scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("ВВедите контактный телефон: ");
+        String phone = scanner.nextLine();
+        System.out.print("Введите адрес доставки: ");
+        String adress = scanner.nextLine();
+        System.out.printf("Ваш заказ принят.\n" + "Товар: %s, в количестве: %d, прибудет к вам в ближайшее время.\n" +
+                "Товар доставим по адресу %s. Ваш контактный номер %s. ", product, quantity, adress, phone);
+        scanner.close();
     }
 }
